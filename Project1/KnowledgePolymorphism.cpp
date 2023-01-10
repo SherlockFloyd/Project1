0cda610c542d091b2feb2f3971059b9a3d49822f
sherlockfloyd <sherlockfloyd@outlook.com>
2023-01-11 00:58:44 +0800

    多态与运算符重载学习完成，类函数中临时对象返回问题解决，单目运算符重载问题解决，同类中静态对象有问题

--- Project1/KnowledgePolymorphism.cpp
+++ Project1/KnowledgePolymorphism.cpp
@@ -43,7 +43,7 @@ virtual 函数返回类型 函数名（参数表）
 {
 	函数体
 }
-在某基类中声明为virtual并在一个或多个派生类中被重新定义的同名成员函数，称为虚函数。
+在某基类中声明为virtual并在一个或多个派生类中被重新定义的同名成员函数，称为虚函数。虚函数必须在类内声明，不能在类外声明
 
 虚函数的用途：
 	实现运行时的多态性，即通过指向派生类的基类指针，访问派生类中同名覆盖成员函数（实现运行时动态绑定，即同一指针运行过程中指向不同的函数）
@@ -104,6 +104,46 @@ void mobile_Invoke()
 	p4.showinfo();
 	mobile& p5 = cdma;  // 以cdma类对象初始化mobile类引用
 	p5.showinfo();
+}
+
+// 虚析构函数
+/*
+
+*/
+// 基类
+class Base
+{
+public:
+	int x;
+	virtual void f() { cout << "base class\n"; };
+	virtual void show() { cout << "x=" << x << endl; };
+	virtual ~Base() { cout << "destructor base class\n"; }  // 虚析构函数
+															// 定义虚析构函数实现通过基类指针释放派生类对象空间
+															// 在本例中，若基类构造函数不加virtual，因仅有delete p,p为基类指针，故当程序结束时仅调用基类析构函数，不会调用派生类的析构函数（认为没有创建派生类的对象），
+															// 但在调用函数中创建的是派生类的对象空间，导致派生类对象空间未能释放
+															// 实现虚析构函数后系统会调用实际指向的对象类析构函数
+};
+// 派生类
+class Derived : public Base
+{
+public:
+	int y;
+	virtual void f() {
+		cin >> y;
+		cout << "Derived class\n";
+	}
+	virtual void show() { Base::show(); cout << "y=" << y << endl; }
+	~Derived() { cout << "destructor derived class\n"; }
+};
+void Derived_Invoke()
+{
+	Base* p;  // 创建基类指针
+	p = new Derived;  // 动态申请对象空间，基类指针指向派生类，基类函数为虚函数，以下调用的函数均为该指针指向的派生类的函数
+	cin >> p->x;  // 输入x
+	p->f();  // 输入y
+	p->show();
+	delete p;
+
 }
 
 // 抽象类
@@ -192,6 +232,7 @@ void Shape_Invoke()
 {
 	函数体
 }
+双目运算符的重载：x + y <--> x.(+)(y)
 */
 
 // 利用运算符重载实现复数的运算和判等
@@ -200,11 +241,13 @@ class Complex
 {
 private:
 	double real, imag;  // 实部，虚部
+	//static Complex temp;  // 静态类编译出错
 public:
 	Complex(double r = 0, double i = 0) : real(r), imag(i) { }  // 构造函数
 	double Real() { return real; }  // 获得实部
 	double Imag() { return imag; }  // 获得虚部
-	Complex operator +(Complex&);  // 虚数加法，+重载
+	void print();
+	Complex operator +(Complex);  // 虚数加法，+重载
 	Complex operator +(double);  // 虚数加实数，+重载
 	bool operator ==(Complex);  // 判等，==重载
 	Complex operator ++();  // 单目运算符 x++ 重载
@@ -212,7 +255,7 @@ public:
 	~Complex() { };  // 析构函数
 };
 // 重载运算符 ＋，两边是虚数对象
-Complex Complex::operator + (Complex& c) 
+Complex Complex::operator + (Complex c) 
 {
 	Complex temp;
 	temp.real = real + c.real;
@@ -235,41 +278,85 @@ bool Complex::operator ==(Complex c)
 	else
 		return false;
 }
-// 前置运算符定义
+// 前置++运算符定义，先+1后输出
 Complex Complex::operator ++ ()
 {
 	real += 1;
 	return *this;
 }
-// 后置运算符定义
+// 后置++运算符定义，先输出后+1
 Complex Complex::operator ++ (int)
 {
-	real += 1;
-	return *this;
+	Complex temp = *this;
+	++real;  // 利用前置实现后置，减少代码重复
+	return temp;
+}
+/*
+单目运算符重载知识点：
+
+1、形参的区别及原因
+	前置++没有形参，而后置++有一个int形参，但是该形参也没有被用到。其目的只是为了绕过语法的限制。
+	前置++与后置++的操作符重载函数，函数原型必须不同。否则就违反了“重载函数必须拥有不同的函数原型”的语法规定。
+	虽然前置++与后置++的返回类型不同，但是返回类型不属于函数原型。为了绕过语法限制，只好给后置++增加了一个int形参。
+2、代码实现的区别
+	前置++的实现比较简单，自增之后，将*this返回即可。需要注意的是，一定要返回*this。
+	后置++的实现稍微麻烦一些。因为要返回自增之前的对象，所以先将对象拷贝一份，再进行自增，最后返回那个拷贝。
+3、效率的区别
+	如果不需要返回自增之前的值，那么前置++和后置++的计算效果都一样。但是，我们仍然应该优先使用前置++，尤其是对于用户自定义类型的自增操作。
+	前置++的效率更高，理由是：后置++会生成临时对象。
+	很明显，tmp是一个临时对象，会造成一次构造函数和一次析构函数的额外开销。虽然，编译器在某些情况下可以优化掉这些开销。但是，我们最好不要依赖编译器的行为。
+	所以，在非内置类型的时候，尽量使用前置++，因为效率高（后置自增，效率低）
+*/
+
+
+// 打印输出结果
+void Complex::print()
+{
+	cout << real << "+" << imag << "i";
+	cout << endl;
 }
 // 运算符重载测试函数调用
 void Complex_Invoke()
 {
 	Complex c1(3, 4), c2(5, 6), c3;  // 创建三个复数对象
-	cout << "C1 = " << c1.Real() << "+j" << c1.Imag() << endl;  
-	cout << "C2 = " << c2.Real() << "+j" << c2.Imag() << endl;
+	cout << "C1 = ";
+	c1.print();
+	cout << "C2 = ";
+	c2.print();
 	c3 = c1 + c2;  // 使用重载运算符 + 实现两个虚数的加法
-	cout << "C3 = " << c3.Real() << "+j" << c3.Imag() << endl;
+	cout << "C3 = ";
+	c3.print();
 	c3 = c3 + 6.5;  // 使用重载运算符 + 实现虚数和实数的加法
-	cout << "C3 + 6.5 = " << c3.Real() << "+j" << c3.Imag() << endl;
+	cout << "C3 + 6.5 = ";
+	c3.print();
 	if (c1 == c2)  // 使用重载运算符 == 判两个虚数是否相等
-		cout << "两个复数相等";
+		cout << "两个复数相等" << endl;
 	else
-		cout << "两个复数不相等";
+		cout << "两个复数不相等" << endl;
+
+	c3 = c1++;  // 后置
+	c3.print();
+	c3 = ++c1;  // 前置
+	c3.print();
 }
-
-
-
-
+ // 深拷贝与浅拷贝
+/*
+浅拷贝（shallowcopy）
+	只复制直接成员变量的值
+深拷贝（deepcopy）
+	同时复制资源 （如指针指向的空间）
+浅接贝的运算
+	赋值运算符（=）、系统默认的拷贝构造函数实现的都是浅拷贝
+实现深接贝
+	自定义一个成员函数
+	重载拷贝构造函数（也叫复制构造函数）
+	重载赋值运算符
+*/
 
 void PolymorphismEntrance()
 {
 	//mobile_Invoke();
 	//Shape_Invoke();
-	Complex_Invoke();
+	//Complex_Invoke();
+	Derived_Invoke();
 }
