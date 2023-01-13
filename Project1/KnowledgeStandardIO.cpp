94832e72f1e7f219f627d7158b9fb178521777a3
sherlockfloyd <sherlockfloyd@outlook.com>
2023-01-14 00:53:30 +0800

    文件操作学习（中），实例学习

--- Project1/KnowledgeStandardIO.cpp
+++ Project1/KnowledgeStandardIO.cpp
@@ -1,12 +1,19 @@
 #pragma warning(disable:4996)  // 忽略警告提醒
 
 #include<iostream>
+#include<fstream>
 #include<windows.h>
 #include<iomanip>
 #include<cstring>
+#include<cstdlib>
+#include<ctime>
+
 #include "KnowledgeStandardIO.h"
 
 using namespace std;
+
+#define SUIT_NUMBER 4
+#define FACE_NUMBER 13  // 扑克牌实例中用到
 
 
 // 流及流库
@@ -271,6 +278,347 @@ void open (const char * filename, openmode mode)
 mode 
 */
 
+// 文本文件的读写
+void filewrite()
+{
+	//打开文件
+	ofstream out(".\\textfile\\text_file.txt");  // 以写文件模式打开，out是对象名，使用ofstream类的构造函数打开文件，等价于以下写法
+	/*ofstream out1;
+	out1.open("text_file.txt");*/
+	if (!out) {
+		cout << "打开文件失败！" << endl;
+		return ;
+	}
+	//写文件
+	out << "Welcome to ";  // 使用流插入运算符 << 输出到文件中
+	char ch[] = "Xi'an Jiaotong University.";
+	for (int i = 0; ch[i] != 0; i++)
+		out.put(ch[i]);   // 逐个字符输入，直至字符串结束，使用put成员函数输入到文件中
+	out.close(); //关闭文件
+}
+void fileread()
+{
+	//打开文件
+	ifstream in(".\\textfile\\text_file.txt");  // 以读文件模式打开，in是对象名，使用ifstream类的构造函数打开文件
+	if (!in){
+		cout << "不可以打开文件" << endl;
+		return ;
+	}
+	//读文件
+	char ch[80];
+	in >> ch; //用 >> 读取第一个单词Welcome，与cin相同，以空格和回车作为分隔符，使用流插入运算符 >> 输入到空间中
+	cout << ch;
+	in >> ch; //读取第二个单词to 
+	cout << ch;
+	while (in)  {  //剩余部分用get函数读出并显示
+		char c = in.get();
+		if (in)   // 为何加if判断原因如下
+			cout << c;
+	}
+	/*
+	如何判断到达文件末尾：
+	读取指针工作步骤：先将指向的内容输出到空间中，然后指针后移，对象的返回值是否为真由本次输出的内容决定，而不是当前指针指向的内容。
+	当没有读到文件尾部时，ifstream 对象 in 相当于一个true值，甚至直到读取完所有有效数据后，in仍然相当于true。这时再读取一次文件后，in才变成了相当于false值的对象。
+	*/
+	in.close(); //关闭文件
+}
+// 成绩文件计算平均分并输出
+void GradeCompute()
+{
+	char ch[20];
+	double math, eng, phy;
+	double avg;
+	ifstream fin(".\\textfile\\grade_flie.txt");
+	ofstream fout(".\\textfile\\grade_result_file.txt");
+	if (!fin || !fout) {
+		cout << "文件打开失败！" << endl;
+		return;
+	}
+	while (fin) {
+		fin >> ch >> math >> eng >> phy;  // 按行读取，一次读取一行元素，读取完之后读取指针自动指向下一行第一个有效位置
+		if (fin) {
+			avg = (math + eng + phy) / 3;
+			fout << ch << '\t' << math << '\t' << eng << '\t' << phy << '\t' << avg << endl;
+		}
+	}
+}
+
+// 二进制文件的读写
+/*
+对二进制文件进行操作时，打开文件要指定方式 ios::binary
+从二进制文件输入数据可调用istream流类提供的成员函数，
+函数原型为：
+	istream& read(char* buffer, int len)
+向二进制文件输出数据可调用ostream流类提供的成员函数，
+函数原型为：
+	ostream& write(const char* buffer,int len)
+
+	两个函数的格式相同：
+	第一个参数：字符指针，用于指向输入输出数据所存放的内存空间地址
+	第二个参数：整数，表示要输入输出的数据的字节数
+*/
+
+// 二进制文件的顺序读写、随机读写
+/*
+istream 类操作流读指针的成员函数:
+1、istream & istream :: seekg ( long pos) ;
+	读指针从流的起始位置向后移动由pos指定字节
+2、istream & istream :: seekg ( long off, ios::seek_dir );
+	读指针从流的 seek_dir 位置移动 , off 指定字节
+3、long & istream :: tellg ();
+	返回读指针当前所指位置值
+
+	ios::seek_dir 值：
+	cur		当前读指针所在的当前位置
+	beg		文件流的开始位置 （文件头部）
+	end		文件流的结尾处
+*/
+class Student // 定义类 
+{
+	char Name[10]; //*Name;
+	char Class[10]; //*Class;
+	char Sex;
+	int Age;
+public:
+	Student() { }
+	Student(char* Name, char* Class, char sex, int age)
+	{
+		//this->Name = new char[strlen(Name) + 1];
+		//this->Class = new char[strlen(Class) + 1];
+		strcpy(this->Name, Name);
+		strcpy(this->Class, Class);
+		Sex = sex;
+		Age = age;
+	}
+	void Showme()
+	{
+		cout << *Name << '\t' << *Class << '\t' << Sex << '\t' << Age << endl;
+	}
+	~Student() {
+		//delete[] Name;
+		//delete[] Class;
+	}
+};
+void Binaryfile()
+{
+	Student stu[3] = {
+	Student("wang","dianqi11",'m',27),
+	Student("liu","jixie01",'f',24),
+	Student("li","shengwu12",'m',39) 
+	};  // 创建一个二维数组，存放三个学生类stu[i]分别为三个第二维数组（学生类）的首地址指针，第一维中存放三个首地址指针
+	//打开文件
+	ofstream file1("binary_file.dat", ios::binary);  // 以二进制形式打开文件
+	if (!file1) {
+		cout << "文件打开失败!"; 
+		return;
+	}
+	//写文件
+	for (int i = 0; i < 3; i++)
+		file1.write((char*)&stu[i], sizeof(stu[i]));  // 将类指针强制转换为字符指针（指针指向的类空间大小不变），以一个类的大小输出到文件中
+	file1.close(); //关闭文件
+	///////以下为读文件并显示出来//////////
+	Student stu2; //建立对象
+	//打开文件
+	ifstream file2("binary_file.dat", ios::binary);
+	if (!file2) {
+		cout << "文件打开失败!"; 
+		return;
+	}
+	//读文件
+	while (file2)
+	{
+		file2.read((char*)&stu2, sizeof(stu2));  // 将指向一个类大小的指针强制转换为字符指针，并接收文件读取的内容，大小为一个类的大小
+		if (file2) 
+			stu2.Showme();
+	}
+	//实现随机读取，反向输出学生信息
+	file2.seekg(0, ios::end);  // 定位文件指针到文件末尾
+	int len = file2.tellg();  // 得到文件位置，返回整数（文件大小）
+	if (len <= 0) {
+		cout << "文件指针定位失败！";
+		return;
+	}
+	for (int i = len / sizeof(stu2) - 1; i >= 0; i--) {  // 以一个类大小为单位向前移动指针
+		file2.seekg(i * sizeof(stu2));
+		file2.read((char*)&stu2, sizeof(stu2));
+		stu2.Showme();
+	}
+	//关闭文件
+	file2.close();
+}
+
+
+// 文件操作实例
+/*====================================================================================================*/
+// 扑克随机洗牌结果保存到文件
+// 扑克类
+class Card 
+{
+public:
+	string suit; // 花色
+	string face; // 面值
+};
+// 扑克管理类
+class CardManager
+{
+private:
+	Card deck[SUIT_NUMBER][FACE_NUMBER];
+public:
+	CardManager();
+	void shuffle(); // 洗牌
+	void deal(); //发牌
+};
+CardManager::CardManager()
+{
+	string suit[] = { "红桃","方块", "梅花", "黑桃" };
+	string face[] = {
+		"A","2","3","4","5","6","7","8","9","10","J","Q","K"
+	};
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 13; j++) {
+			deck[i][j].suit = suit[i];
+			deck[i][j].face = face[j];
+		}
+	}
+}
+void CardManager::shuffle()
+{
+	srand(time(NULL)); // 随机函数初始种子
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 13; j++) {
+			int m = rand() % 4; // 随机函数
+			int k = rand() % 13; // 随机函数
+			Card temp = deck[i][j];
+			deck[i][j] = deck[m][k];
+			deck[m][k] = temp;
+			// 通过随机数组交换实现洗牌
+		}
+	}
+}
+void CardManager::deal()
+{
+	char* person[4] = { "张三","李四","王五","赵六" };
+	ofstream poker;
+	poker.open(".\\textfile\\poker_file.txt");
+	for (int i = 0; i < 4; i++) {
+		poker << person[i] << "的牌：" << endl;
+		for (int j = 0; j < 13; j++) {
+			poker << "第" << j + 1 << "张：  "
+				<< deck[i][j].suit << deck[i][j].face << "\t\t";
+			if ((j + 1) % 4 == 0)
+				poker << endl;
+		}
+		poker << endl << endl;
+	}
+	poker.close();
+}
+// 扑克自动发牌调用函数
+void CardPlay_invoke()
+{
+	CardManager player;
+	player.shuffle();
+	player.deal();
+}
+
+
+// 统计文件单词词频
+class wordtype
+{
+public:
+	char word[20]; 
+	int count;
+};
+
+int getwords(wordtype* words)
+{
+	ifstream wordsfile(".\\textfile\\words_source.txt"); // 打开英文文件 
+	if (!wordsfile) {
+		cout << "文件打开错误！" << endl;
+		return 1;
+	}
+
+	int n = 0;  // 单词数量
+	char word[20];  // 存储每个单词
+	int m;  // 单词在数组中位置
+	while (wordsfile) {
+		wordsfile >> word; // 读单词 
+		if (!wordsfile) { // 文件结尾时退出循环 
+			break;
+		}
+		bool flag = false;
+		for (m = 0; m < n; m++) {
+			if (!strcmp(word, words[m].word)) { // 已存在该单词 
+				words[m].count++; // 原有单词计数加1 
+				flag = true;
+				break;
+			}
+		}
+		if (!flag) { // 发现新单词 
+			words[m].count = 1; // 新单词计数为1 
+			strcpy(words[m].word, word); // 保存新单词 
+			n++; // 总单词计数加1 
+		}
+	}
+	wordsfile.close(); // 关闭文件 
+	return n; // 返回单词个数 
+}
+// 单词词频统计函数调用
+void words_invoke()
+{
+	wordtype words[200] = { "",0 };  // 单词结构体变量定义与初始化 
+	int n = getwords(words); // 调用获取单词的函数 
+	cout << "英文单词统计结果如下：" << endl;
+	for (int m = 0; m < n; m++)
+		cout << words[m].word << ':' << words[m].count << endl;
+	cout << "搜索出共" << n << "个单词。" << endl;
+}
+
+
+// 复制MP3文件
+bool mp3cpy(const char* szDestFile, const char* szOrigFile)
+{
+	ofstream d_music(szDestFile, ios::binary); // 以二进制方式打开目标文件 
+	ifstream s_music(szOrigFile, ios::binary); // 以二进制方式打开原始文件 
+
+	bool bRet = true;  // 复制成功与否的标志
+	if (s_music.bad()) { // 原始文件出错 
+		bRet = false;
+	}
+	else {
+		s_music.seekg(0L, ios::beg); // 定位原始文件开始处 
+		while (!s_music.eof()) { // 原始文件未结尾 
+			char szBuf[256] = { 0 };
+			s_music.read(szBuf, sizeof(char) * 256); // 每次读取原始文件最多256字节 
+			int length = s_music.gcount(); // 实际读取的字节数 
+			if (d_music.bad()) { // 目标文件出错 
+				bRet = false;
+				break;
+			}
+			d_music.write(szBuf, length); // 追加模式，每次写入目标文件length个字节 
+		}
+	}
+	d_music.close(); // 关闭目标文件 
+	s_music.close(); // 关闭原始文件 
+
+	return bRet;
+}
+// 复制MP3文件调用
+void mp3cpy_invoke()
+{
+	char szOrigFile[50]; // 原始文件名 
+	char szDestFile[50]; // 目标文件名 
+	cout << "请输入原始文件名和目标文件名：";
+	cin >> szOrigFile;
+	cin >> szDestFile;
+
+	bool bRet = mp3cpy(szDestFile, szOrigFile); // 调用文件复制函数 
+	if (bRet) {
+		cout << "文件复制成功！" << endl;
+	}
+	else {
+		cout << "文件复制失败！" << endl;
+	}
+}
 
 
 
@@ -280,5 +628,12 @@ void StandardIOEntrance()
 	//get_input2();
 	//getline_input1();
 	//MemberFunctions_output();
-	ControlSymbols_output();
+	//ControlSymbols_output();
+	//filewrite();
+	//fileread();
+	//GradeCompute();
+	//Binaryfile();
+	//CardPlay_invoke();
+	//words_invoke();
+	mp3cpy_invoke();
 }
